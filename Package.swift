007e3949953e44fc4a835a36c4dc23553280c777
Łukasz Komorowski <lukasz.komorowski.sp@lhsystems.com>
2023-10-24 16:59:32 +0200

    fix

--- Package.swift
+++ Package.swift
@@ -34,7 +34,7 @@ let package = Package(
         .library(name: "Fastis", targets: ["Fastis"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/patchthecode/JTAppleCalendar", from: "8.0.3"),
+        .package(url: "https://github.com/patchthecode/JTAppleCalendar", from: "8.0.5"),
         .package(url: "https://github.com/SnapKit/SnapKit", from: "5.0.1"),
         .package(url: "https://github.com/ilia3546/PrettyCards", from: "1.0.4")
     ],
